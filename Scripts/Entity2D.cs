c9e1862fb7a7f8858dff7a3ea9db358631f27c9e
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-06-06 02:50:11 -0500

    2d sprites

--- Scripts/Entity2D.cs
+++ Scripts/Entity2D.cs
@@ -3,11 +3,10 @@ using System;
 
 public partial class Entity2D : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	[Export] float Speed = 300.0f;
+	[Export] float JumpVelocity = -400.0f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	[Export] float gravity = 20f;
 
 	public override void _PhysicsProcess(double delta)
 	{
