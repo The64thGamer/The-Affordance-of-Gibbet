1f96ed6c9766025e4ed35bb87e039de1a77e1733
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-06-07 00:16:59 -0500

    Improved sprites and updater script, added anims

--- Scripts/Entity.cs
+++ Scripts/Entity.cs
@@ -1,12 +1,16 @@
 using Godot;
 using System;
 
-public partial class Entity2D : CharacterBody2D
+public partial class Entity : CharacterBody2D
 {
+	[Export] SpriteAnim sprite;
 	[Export] float Speed = 300.0f;
 	[Export] float JumpVelocity = -400.0f;
 
 	[Export] float gravity = 20f;
+	
+	[Export] float walkAnimSpeed;
+	float walkTimer;
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -34,5 +38,45 @@ public partial class Entity2D : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+		UpdateSprites(delta);
+	}
+
+	void UpdateSprites(double delta)
+	{
+		sprite.FlipH = Velocity.X < 0 ? true : false;
+		if(Velocity.Y == 0)
+		{
+			if(Velocity == Vector2.Zero)
+			{
+				sprite.SetSprite("Idle");
+				walkTimer = 0;
+			}
+			if(Velocity.X != 0)
+			{
+				switch (Mathf.FloorToInt(walkTimer))
+				{
+					case 0:
+						sprite.SetSprite("Walk C");
+					break;
+					case 1:
+						sprite.SetSprite("Walk B");
+					break;
+					case 2:
+						sprite.SetSprite("Walk A");
+					break;
+					case 3:
+						sprite.SetSprite("Walk B");
+					break;
+					default:
+					walkTimer = 0;
+					break;
+				}
+				walkTimer += (float)delta * walkAnimSpeed;
+			}
+		}
+		else
+		{
+			sprite.SetSprite("Jump");
+		}
 	}
 }
--- Scripts/SpriteAnim.cs
+++ Scripts/SpriteAnim.cs
@@ -5,13 +5,35 @@ using System.Linq;
 
 public partial class SpriteAnim : Sprite2D
 {
-	[Export] string[] spriteNames;
-	Dictionary<string,int> spriteHash;
+	Dictionary<string,int> spriteHash = new Dictionary<string, int>();
 
-	const int horizontalItems = 10;
+	static string[] spritePlayer = new string[]{
+		"Idle",
+		"Walk A","Walk B","Walk C",
+		"Jump",
+		};
+	static string[] spriteUI = new string[]{
+		"Button B",
+		"Button Left Arrow",
+		"Button Up Arrow",
+		"Button Down Arrow",
+	};
 
 	public override void _Ready()
 	{
+		string[] spriteNames = new string[0];
+		switch (Texture.ResourcePath.GetFile())
+		{
+			case "Sprite Player.png":
+				spriteNames = spritePlayer;
+				break;
+			case "Sprite UI.png":
+				spriteNames = spriteUI;
+				break;
+			default:
+				GD.PrintErr("Unknown Sprite Set: " + Texture.ResourcePath.GetFile());
+				break;
+		}
 		int e;
 		for (int i = 0; i < spriteNames.Length; i++)
 		{
@@ -25,6 +47,9 @@ public partial class SpriteAnim : Sprite2D
 		if(spriteHash.TryGetValue(name,out int index))
 		{
 			Frame = index;
+			return;
 		}
+
+		GD.PrintErr("Unknown Sprite '" + name + "'");
 	}
 }
