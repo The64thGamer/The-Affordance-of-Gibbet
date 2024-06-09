d8c78ddac1333bc18684e61f2ecbbeb94ddc9344
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2024-06-09 00:26:53 -0500

    Made camera better

--- Scripts/PlayerCam.cs
+++ Scripts/PlayerCam.cs
@@ -7,10 +7,12 @@ public partial class PlayerCam : Camera2D
 	[Export] float cameraLookaheadMultiplier = 1;
 	[Export] float cameraSpeed = 0.23f;
 	[Export] float maxDeadzoneX = 10;
+	[Export] Curve camSpeedCurve;
 
 	Vector2 deadzonePoint;
 	Vector2 targetPoint;
-	
+	bool oldDeadZone = false;
+	float camRampupTimer = 0;
 
 	public override void _Ready()
 	{
@@ -21,24 +23,25 @@ public partial class PlayerCam : Camera2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if(player.Velocity.X == 0)
-		{
-			targetPoint.X = GlobalPosition.X;
-		}
 
 		Vector2 distance = new Vector2(player.GlobalPosition.X - deadzonePoint.X,player.GlobalPosition.Y - deadzonePoint.Y);
+		camRampupTimer += (float)delta;
 
 		if(distance.X > maxDeadzoneX)
-		{		
+		{	
+			if(!oldDeadZone){camRampupTimer = 0;}
+			oldDeadZone = true;	
 			deadzonePoint.X = player.GlobalPosition.X - maxDeadzoneX+1;
 			targetPoint.X = player.GlobalPosition.X + (maxDeadzoneX * cameraLookaheadMultiplier);
 		}
-		if(distance.X < -maxDeadzoneX)
-		{		
+		else if(distance.X < -maxDeadzoneX)
+		{	
+			if(oldDeadZone){camRampupTimer = 0;}
+			oldDeadZone = false;
 			deadzonePoint.X = player.GlobalPosition.X + maxDeadzoneX-1;
 			targetPoint.X = player.GlobalPosition.X + (-maxDeadzoneX * cameraLookaheadMultiplier);
 		}
 
-		GlobalPosition = GlobalPosition.Lerp(targetPoint,Mathf.Pow(Mathf.Min(1,Mathf.Abs(targetPoint.X - GlobalPosition.X) * cameraSpeed* (float)delta),2));
+		GlobalPosition = GlobalPosition.Lerp(targetPoint,camSpeedCurve.SampleBaked(camRampupTimer) * Mathf.Min(1,Mathf.Abs(targetPoint.X - GlobalPosition.X) * cameraSpeed* (float)delta));
 	}
 }
